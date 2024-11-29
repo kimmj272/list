328a32de1c79e7795a86a18ca9b728e572ab065f
kimmj272 <rlaaudwls5220@gmail.com>
2024-11-29 23:00:05 +0900

    list

---
+++ ListTest/ListTest/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ListTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "출력결과";
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtResult.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.txtResult.Location = new System.Drawing.Point(25, 69);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(533, 435);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.Text = "출력";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(74, 548);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(141, 46);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(338, 548);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(141, 46);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Clear";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 629);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "frmMain";
+            this.Text = "ListTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtResult;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ ListTest/ListTest/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListTest
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            //TestArray();
+            //TestArray2();
+            //TestArray3();
+            TestList();
+            TestList2();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+        }
+
+        public void TestArray()
+        {
+            txtResult.Text = "";
+
+            int[] aTemp = new int[3];
+            aTemp[0] = 100;
+            aTemp[1] = 200;
+            aTemp[2] = 300;
+
+            for (int i = 0; i < aTemp.Length; i++)
+            {
+                string sLog = string.Format("Temp[{0}] = {1}", i, aTemp[i]);
+                Console.WriteLine(sLog);
+                txtResult.Text += sLog + "\n";
+            }
+
+            txtResult.Text += "----------------------------\n";
+        }
+
+        public void TestArray2()
+        {
+            int[] aTemp = new int[3];
+            aTemp[0] = 100;
+            aTemp[1] = 200;
+            aTemp[2] = 300;
+
+            int nIndex = 0;
+            while (nIndex < aTemp.Length)
+            {
+                string sLog = string.Format("Temp[{0}] = {1}", nIndex, aTemp[nIndex]);
+                Console.WriteLine(sLog);
+                txtResult.Text += sLog + "\n";
+                nIndex++;
+            }
+
+            txtResult.Text += "----------------------------\n";
+        }
+
+        public void TestArray3()
+        {
+            int[] aTemp = new int[3];
+            aTemp[0] = 100;
+            aTemp[1] = 200;
+            aTemp[2] = 300;
+
+            int nIndex = 0;
+            do
+            {
+                string sLog = string.Format("Temp[{0}] = {1}", nIndex, aTemp[nIndex]);
+                Console.WriteLine(sLog);
+                txtResult.Text += sLog + "\n";
+                nIndex++;
+            }
+            while (nIndex < aTemp.Length);
+
+            txtResult.Text += "----------------------------\n";
+        }
+
+        public void TestList()
+        {
+            txtResult.Text = "";
+
+            List<int> list = new List<int>();
+            list.Add(100);
+            list.Add(200);
+            list.Add(300);
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                string sLog = string.Format("list[{0}] = {1}", i, list[i]);
+                Console.WriteLine(sLog);
+                txtResult.Text += sLog + "\n";
+            }
+
+            txtResult.Text += "----------------------------\n";
+        }
+
+        public void TestList2()
+        {
+            List<int> list = new List<int>();
+            list.Add(100);
+            list.Add(200);
+            list.Add(300);
+
+            int i = 0;
+            foreach(int value in list)
+            {
+                string sLog = string.Format("list[{0}] = {1}", i, value);
+                Console.WriteLine(sLog);
+                txtResult.Text += sLog + "\n";
+                i++;
+            }
+
+            txtResult.Text += "----------------------------\n";
+        }
+    }
+}
